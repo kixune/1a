fc046e4273a504e663e31f916f58389ca0a4d342
Barnabas Chan <barny@post.com>
2016-07-10 17:20:04 +0800

    add a way to prevent questions from repeating twice

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,12 @@ function pair(q, a) {
   this.q = q;
   this.a = a;
 }
-
 var pair1 = new pair("Questions can be answers?", true);
 var pair2 = new pair("\"Peter Piper picked a peck of pickled peppers.\"<br><br>Perhaps, philosophically, Peter is a pompous prick and a perpetual kleptomanic.", true);
 var pair3 = new pair("(typeof \"true\" == \"string\");<br>Returns true.<br> True?<br>hint: Think through.", true);
 var pair4 = new pair("\"Bacon\"", true);
-var pair5 = new pair("According to Led Zeppelin:<br><br>If there is a bustle in your hedgerow, you should not be alarmed.", true);    var pair6 = new pair("\"Barny creates quiz,<br>The quiz works and Barny lives,<br>Barny completes 1a.\"<br><br>Is the above a haiku?", false);
+var pair5 = new pair("According to Led Zeppelin:<br><br>If there is a bustle in your hedgerow, you should not be alarmed.", true);
+var pair6 = new pair("\"Barny creates quiz,<br>The quiz works and Barny lives,<br>Barny completes 1a.\"<br><br>Is the above a haiku?", false);
 var pair7 = new pair("Short is shorter than Long if we assume they are both strings and are comparing their length.", false);
 var pair8 = new pair("Friend or faux?<br>Therein, lies the rub", false);
 var pair9 = new pair("This quiz, it is good, no?<br>Now now, never suck-up to win!", false);
@@ -28,15 +28,18 @@ var pair10 = new pair("\"Work it harder, make it better,<br>do it faster, makes 
 
 var qA = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8, pair9, pair10];
 
+
 var random = 0;
 var question = "";
 var answer = true;
 $(document).ready(function () {
 
 function nextQ() {
-   random = Math.floor(Math.random() * 10);
+    console.log(qA);
+   random = Math.floor(Math.random() * qA.length);
    question = qA[random].q;
    answer = qA[random].a;
+   qA.splice(random, 1);
    var displayQ = $("#ask");
    displayQ.html(question);
 }nextQ();
@@ -77,6 +80,7 @@ function reset() {
 playerOneCorrect = 0;
 playerTwoCorrect = 0;
 totalTurns = 1;
+qA = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8, pair9, pair10];
 }
 
 function checkWinner() {
