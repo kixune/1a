34b3d64adb5ddb379eaba254c2a9374f6469e1c8
Barnabas Chan <barny@post.com>
2016-07-11 12:37:30 +0800

    add more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,23 +14,29 @@
 
 $(document).ready(function () {
 
-  function pair(q, a) {
+  function Pair(q, a) {
     this.q = q;
     this.a = a;
   }
-  var pair1 = new pair("Questions can be answers?", true);
-  var pair2 = new pair("\"Peter Piper picked a peck of pickled peppers.\"<br><br>Perhaps, philosophically, Peter is a pompous prick and a perpetual kleptomanic.", true);
-  var pair3 = new pair("(typeof \"true\" == \"string\");<br>Returns true.<br> True?<br>hint: Think through.", true);
-  var pair4 = new pair("\"Bacon\"", true);
-  var pair5 = new pair("According to Led Zeppelin:<br><br>If there is a bustle in your hedgerow, you should not be alarmed.", true);
-  var pair6 = new pair("\"Barny creates quiz,<br>The quiz works and Barny lives,<br>Barny completes 1a.\"<br><br>Is the above a haiku?", false);
-  var pair7 = new pair("Short is shorter than Long if we assume they are both strings and are comparing their length.", false);
-  var pair8 = new pair("Friend or faux?<br>Therein, lies the rub", false);
-  var pair9 = new pair("This quiz, it is good, no?<br>Now now, never suck-up to win!", false);
-  var pair10 = new pair("\"Work it harder, make it better,<br>do it faster, makes us stronger.\"<br><br>Is the official motto of the International Bakers Society.", false);
-  
+  var pair1 = new Pair("Questions can be answers?", true);
+  var pair2 = new Pair("\"Peter Piper picked a peck of pickled peppers.\"<br><br>Perhaps, philosophically, Peter is a pompous prick and a perpetual kleptomanic.", true);
+  var pair3 = new Pair("(typeof \"true\" == \"string\");<br>Returns true.<br> True?<br>hint: Think through.", true);
+  var pair4 = new Pair("\"Bacon\"", true);
+  var pair5 = new Pair("According to Led Zeppelin:<br><br>If there is a bustle in your hedgerow, you should not be alarmed.", true);
+  var pair6 = new Pair("\"Barny creates quiz,<br>The quiz works and Barny lives,<br>Barny completes 1a.\"<br><br>Is the above a haiku?", false);
+  var pair7 = new Pair("Short is shorter than Long if we assume they are both strings and are comparing their length.", false);
+  var pair8 = new Pair("Friend or faux?<br>Therein, lies the rub", false);
+  var pair9 = new Pair("This quiz, it is good, no?<br>Now now, never suck-up to win!", false);
+  var pair10 = new Pair("\"Work it harder, make it better,<br>do it faster, makes us stronger.\"<br><br>Is the official motto of the International Bakers Society.", false);
+  //add more questions
+  var pair11 = new Pair("The South American branch of General Assembly is located just beside Machu Picchu.", false);
+  var pair12 = new Pair("\"Cold War Kids\" is an organisation founded to benefit children involved in \'The Cold War\'.", false);
+  var pair13 = new Pair("Cat Power<br>aka<br>Charlyn Marie \"Chan\" Marshall<br>wrote a song called<br>\'The Greatest\'.", true);
+  var pair14 = new Pair("____ Grit<br>is a movie directed, written, produced and edited by the Coen Brothers.", true);
 
-  var qA = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8, pair9, pair10];
+
+
+  var qA = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8, pair9, pair10, pair11, pair12, pair13, pair14];
 
 
 //1.
@@ -65,6 +71,7 @@ var answer = true;
 function nextQ() {
   console.log(qA);
  random = Math.floor(Math.random() * qA.length);
+ console.log(random);
  question = qA[random].q;
  answer = qA[random].a;
  qA.splice(random, 1);
@@ -110,6 +117,7 @@ falseButton.click(function() {
   checkWinner();
   whoseTurn();
   nextQ();
+
 });
 
 
@@ -138,7 +146,6 @@ function reset() {
   totalTurns = 1;
   qA = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8, pair9, pair10];
 }
-
 
 
 
